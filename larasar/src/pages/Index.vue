4d562572a49cff1ad7643e9813e98e8bdc55f632
Arief-af <sekolah.arief@gmail.com>
2022-05-22 08:19:12 +0700

    first

---
+++ larasar/src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+   tes
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import axios from 'axios'
+export default defineComponent({
+  name: 'PageIndex',
+  setup(){
+    const data = ref([])
+
+    axios.get('/tes').then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    return {
+      data
+    }
+  }
+})
+</script>
